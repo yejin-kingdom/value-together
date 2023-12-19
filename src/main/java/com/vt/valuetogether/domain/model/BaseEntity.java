1930ab73d842bd43a89e360e98e124b5868c7a5c
junxtar <aa1870088@naver.com>
2023-12-19 15:01:05 +0900

    inital commit

---
+++ src/main/java/com/vt/valuetogether/domain/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.vt.valuetogether.domain.model;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    public LocalDateTime createdAt;
+
+    @LastModifiedDate
+    public LocalDateTime modifiedAt;
+}
---
+++ src/main/java/com/vt/valuetogether/infra/mail/MailConfig.java
@@ -0,0 +1,8 @@
+package com.vt.valuetogether.infra.mail;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MailConfig {
+
+}
