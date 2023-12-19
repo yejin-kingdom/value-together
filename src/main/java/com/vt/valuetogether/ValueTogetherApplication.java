ff1ec5423618da80c7521daa6a8f6e2331a1d2f0
junxtar <aa1870088@naver.com>
2023-12-19 15:00:30 +0900

    first commit

---
+++ src/main/java/com/vt/valuetogether/ValueTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.vt.valuetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValueTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vt/valuetogether/ValueTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vt.valuetogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
