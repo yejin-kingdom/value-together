9102bab3891607d87596028d7cf42953a6bdc5f9
emost22 <emoney96@naver.com>
2023-12-20 21:10:42 +0900

    Feat: add custom responseEntity

---
+++ src/main/java/com/vt/valuetogether/global/response/RestResponse.java
@@ -0,0 +1,38 @@
+package com.vt.valuetogether.global.response;
+
+import com.vt.valuetogether.global.meta.ResultCode;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestResponse<T> implements Serializable {
+
+    private HttpStatus status;
+    private Integer code;
+    private String message;
+    private T data;
+
+    public static <T> RestResponse<T> success(T data) {
+        return RestResponse.<T>builder()
+            .status(ResultCode.SUCCESS.getStatus())
+            .code(ResultCode.SUCCESS.getCode())
+            .message(ResultCode.SUCCESS.getMessage())
+            .data(data)
+            .build();
+    }
+
+    public static <T> RestResponse<T> error(ResultCode resultCode) {
+        return RestResponse.<T>builder()
+            .status(resultCode.getStatus())
+            .code(resultCode.getCode())
+            .message(resultCode.getMessage())
+            .build();
+    }
+}
