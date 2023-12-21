51d945d899319a267fed175a535aed7a853cd6fe
emost22 <emoney96@naver.com>
2023-12-21 14:00:57 +0900

    Test: add get all samples api test

--- src/test/java/com/vt/valuetogether/domain/sample/controller/SampleControllerTest.java
+++ src/test/java/com/vt/valuetogether/domain/sample/controller/SampleControllerTest.java
@@ -2,14 +2,18 @@ package com.vt.valuetogether.domain.sample.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.vt.valuetogether.domain.BaseMvcTest;
 import com.vt.valuetogether.domain.sample.dto.request.SampleSaveReq;
+import com.vt.valuetogether.domain.sample.dto.response.SampleGetRes;
+import com.vt.valuetogether.domain.sample.dto.response.SampleGetResList;
 import com.vt.valuetogether.domain.sample.dto.response.SampleSaveRes;
 import com.vt.valuetogether.domain.sample.servce.SampleService;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -35,5 +39,15 @@ class SampleControllerTest extends BaseMvcTest {
                                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    void 샘플_전체_조회() throws Exception {
+        SampleGetRes sampleGetRes =
+                SampleGetRes.builder().sampleId(1L).title("title").text("text").build();
+        SampleGetResList sampleGetResList =
+                SampleGetResList.builder().sampleGetReses(List.of(sampleGetRes)).total(1).build();
+        when(sampleService.getAllSamples()).thenReturn(sampleGetResList);
+        this.mockMvc.perform(get("/api/v1/sample")).andExpect(status().isOk()).andDo(print());
     }
 }
