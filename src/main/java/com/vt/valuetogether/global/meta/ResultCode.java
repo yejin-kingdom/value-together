c6fe3b056863d55c04be7c1f1bab84971f937911
YONG-COW-HEE <yongcowhee@gmail.com>
2023-12-22 18:41:12 +0900

    feat: Login ResultCode 추가

--- src/main/java/com/vt/valuetogether/global/meta/ResultCode.java
+++ src/main/java/com/vt/valuetogether/global/meta/ResultCode.java
@@ -10,7 +10,11 @@ public enum ResultCode {
     NOT_FOUND_SAMPLE(HttpStatus.NOT_FOUND, 2000, "샘플 데이터가 없습니다."),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, 2000, "존재하지 않는 유저입니다."),
     NOT_FOUND_TEAM(HttpStatus.NOT_FOUND, 2000, "존재하지 않는 팀입니다."),
-    ACCESS_DENY(HttpStatus.FORBIDDEN, 4000, "접근 권한이 없습니다.");
+    ACCESS_DENY(HttpStatus.FORBIDDEN, 4000, "접근 권한이 없습니다."),
+    NOT_VALID_USERNAME_PATTERN(HttpStatus.BAD_REQUEST, 3000, "username 형식에 맞지 않습니다."),
+    NOT_VALID_EMAIL_PATTERN(HttpStatus.BAD_REQUEST, 3000, "email 형식에 맞지 않습니다"),
+    NOT_VALID_PASSWORD_PATTERN(HttpStatus.BAD_REQUEST, 3000, "password 형식에 맞지 않습니다."),
+    DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, 3000, "중복된 username 입니다.");
 
     private HttpStatus status;
     private Integer code;
