bd2ef981f53ffee71d7154211f747dadb54b5a0c
leeyejin <leeyejin113@gmail.com>
2023-12-22 20:13:53 +0900

    Style: 코드 자동 포맷팅

--- src/main/java/com/vt/valuetogether/global/validator/UserValidator.java
+++ src/main/java/com/vt/valuetogether/global/validator/UserValidator.java
@@ -26,19 +26,19 @@ public class UserValidator {
         return user == null;
     }
 
-    private static void validateUsername(String username){
+    private static void validateUsername(String username) {
         if (!Pattern.matches(USERNAME_REGEX, username)) {
             throw new IllegalArgumentException("username 형식에 맞지 않습니다.");
         }
     }
 
-    private static void validatePassword(String password){
+    private static void validatePassword(String password) {
         if (!Pattern.matches(PASSWORD_REGEX, password)) {
             throw new IllegalArgumentException("password 형식에 맞지 않습니다.");
         }
     }
 
-    private static void validateEmail(String email){
+    private static void validateEmail(String email) {
         if (!Pattern.matches(EMAIL_REGEX, email)) {
             throw new IllegalArgumentException("email 형식에 맞지 않습니다.");
         }
