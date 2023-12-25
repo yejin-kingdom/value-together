514dbf09bf71567ea54e8a6bec84e543db07620c
barami62 <wodbs7758@naver.com>
2023-12-25 21:10:36 +0900

    Feat: add UserDetailsServiceImpl

---
+++ src/main/java/com/vt/valuetogether/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.vt.valuetogether.global.security;
+
+import com.vt.valuetogether.domain.user.entity.User;
+import com.vt.valuetogether.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        
+        if (user == null) {
+            throw new IllegalArgumentException("없는 username");
+        }
+
+        return new UserDetailsImpl(user);
+    }
+}
