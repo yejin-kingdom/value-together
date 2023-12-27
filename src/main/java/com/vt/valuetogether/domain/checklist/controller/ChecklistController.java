04b9d401f2aaf506006a78cfdb75036af18f767d
emost22 <emoney96@naver.com>
2023-12-27 16:38:22 +0900

    Feat: add save checklist api

---
+++ src/main/java/com/vt/valuetogether/domain/checklist/controller/ChecklistController.java
@@ -0,0 +1,24 @@
+package com.vt.valuetogether.domain.checklist.controller;
+
+import com.vt.valuetogether.domain.checklist.dto.request.ChecklistSaveReq;
+import com.vt.valuetogether.domain.checklist.dto.response.ChecklistSaveRes;
+import com.vt.valuetogether.domain.checklist.service.ChecklistService;
+import com.vt.valuetogether.global.response.RestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/checklists")
+@RequiredArgsConstructor
+public class ChecklistController {
+    private final ChecklistService checklistService;
+
+    @PostMapping
+    public RestResponse<ChecklistSaveRes> saveChecklist(
+            @RequestBody ChecklistSaveReq checklistSaveReq) {
+        return RestResponse.success(checklistService.saveChecklist(checklistSaveReq));
+    }
+}
