cdb6240eafbcedae8b11ce0ef7a3b871cda568df
YONG-COW-HEE <yongcowhee@gmail.com>
2023-12-28 01:17:37 +0900

    feat: backgroundColor validator 구현 및 팀 삭제 빈 DTO 컨벤션 적용

--- src/main/java/com/vt/valuetogether/global/validator/TeamValidator.java
+++ src/main/java/com/vt/valuetogether/global/validator/TeamValidator.java
@@ -1,18 +1,33 @@
 package com.vt.valuetogether.global.validator;
 
+import static com.vt.valuetogether.global.meta.ResultCode.INVALID_BACKGROUNDCOLOR_PATTERN;
 import static com.vt.valuetogether.global.meta.ResultCode.NOT_FOUND_TEAM;
 
+import com.vt.valuetogether.domain.team.dto.request.TeamCreateReq;
 import com.vt.valuetogether.domain.team.entity.Team;
 import com.vt.valuetogether.global.exception.GlobalException;
+import java.util.regex.Pattern;
 
 public class TeamValidator {
+    private static final String TEAM_BACKGROUNDCOLOR_REGEX = "^#[0-9]$";
+
     public static void validate(Team team) {
         if (!checkIsNull(team)) {
             throw new GlobalException(NOT_FOUND_TEAM);
         }
     }
 
+    public static void validate(TeamCreateReq req) {
+        if (!checkIsValidateTeamBackgroundColor(req.getBackgroundColor())) {
+            throw new GlobalException(INVALID_BACKGROUNDCOLOR_PATTERN);
+        }
+    }
+
     private static boolean checkIsNull(Team team) {
         return team == null;
+    }
+
+    private static boolean checkIsValidateTeamBackgroundColor(String backgroundColor) {
+        return Pattern.matches(TEAM_BACKGROUNDCOLOR_REGEX, backgroundColor);
     }
 }
