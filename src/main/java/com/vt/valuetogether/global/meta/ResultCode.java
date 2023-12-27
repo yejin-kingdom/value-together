08e8a9b141811f7e7c6888ec5ece6e722a2cc7b7
barami62 <wodbs7758@naver.com>
2023-12-27 20:01:22 +0900

    [#25] Refactor: 도메인 별로 예외 구분

--- src/main/java/com/vt/valuetogether/global/meta/ResultCode.java
+++ src/main/java/com/vt/valuetogether/global/meta/ResultCode.java
@@ -1,17 +1,38 @@
 package com.vt.valuetogether.global.meta;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@RequiredArgsConstructor
 public enum ResultCode {
+
+    // 성공 0번대
     SUCCESS(HttpStatus.OK, 0, "정상 처리 되었습니다"),
 
+    // 기타 1000번대
     SYSTEM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 1000, "알 수 없는 에러가 발생했습니다."),
 
-    NOT_FOUND_SAMPLE(HttpStatus.NOT_FOUND, 2001, "샘플 데이터가 없습니다."),
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND, 2002, "존재하지 않는 유저입니다."),
-    NOT_FOUND_TEAM(HttpStatus.NOT_FOUND, 2003, "존재하지 않는 팀입니다."),
+    // 유저 2000번대
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, 2000, "존재하지 않는 유저입니다."),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, 2001, "잘못된 토큰"),
+    EXPIRED_TOKEN(HttpStatus.BAD_REQUEST, 2002, "만료된 토큰"),
+
+    // 팀 3000번대
+    NOT_FOUND_TEAM(HttpStatus.NOT_FOUND, 3000, "존재하지 않는 팀입니다."),
+
+    // 카테고리 4000번대
+
+    // 카드 5000번대
+
+    // 작업자 6000번대
+
+    // 체크리스트 7000번대
+
+    // 할일 8000대
+
+    // 댓글 9000대
 
     INVALID_USERNAME_PATTERN(HttpStatus.BAD_REQUEST, 3001, "username 형식에 맞지 않습니다."),
     INVALID_EMAIL_PATTERN(HttpStatus.BAD_REQUEST, 3002, "email 형식에 맞지 않습니다"),
@@ -20,13 +41,7 @@ public enum ResultCode {
 
     ACCESS_DENY(HttpStatus.FORBIDDEN, 4000, "접근 권한이 없습니다.");
 
-    private HttpStatus status;
-    private Integer code;
-    private String message;
-
-    ResultCode(HttpStatus status, Integer code, String errorMessage) {
-        this.status = status;
-        this.code = code;
-        this.message = errorMessage;
-    }
+    private final HttpStatus status;
+    private final Integer code;
+    private final String message;
 }
