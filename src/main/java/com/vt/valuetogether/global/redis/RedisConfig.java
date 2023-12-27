9271a236433f73f170637c40f4ec9707ec2ecbb8
barami62 <wodbs7758@naver.com>
2023-12-27 16:25:53 +0900

    Refactor: move RedisConfig

--- src/main/java/com/vt/valuetogether/global/redis/RedisConfig.java
+++ src/main/java/com/vt/valuetogether/global/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.vt.valuetogether.global.config;
+package com.vt.valuetogether.global.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
