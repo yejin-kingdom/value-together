d860194be1fa572fb3d74695c0a9c0f8bf0eb824
emost22 <emoney96@naver.com>
2023-12-29 00:56:02 +0900

    [#54] Feat: add update card api

---
+++ src/main/java/com/vt/valuetogether/domain/card/dto/response/CardUpdateRes.java
@@ -0,0 +1,6 @@
+package com.vt.valuetogether.domain.card.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class CardUpdateRes {}
--- src/main/java/com/vt/valuetogether/domain/card/service/impl/CardServiceImpl.java
+++ src/main/java/com/vt/valuetogether/domain/card/service/impl/CardServiceImpl.java
@@ -3,11 +3,14 @@ package com.vt.valuetogether.domain.card.service.impl;
 import static com.vt.valuetogether.infra.s3.S3Util.FilePath.CARD;
 
 import com.vt.valuetogether.domain.card.dto.request.CardSaveReq;
+import com.vt.valuetogether.domain.card.dto.request.CardUpdateReq;
 import com.vt.valuetogether.domain.card.dto.response.CardSaveRes;
+import com.vt.valuetogether.domain.card.dto.response.CardUpdateRes;
 import com.vt.valuetogether.domain.card.entity.Card;
 import com.vt.valuetogether.domain.card.repository.CardRepository;
 import com.vt.valuetogether.domain.card.service.CardService;
 import com.vt.valuetogether.domain.card.service.CardServiceMapper;
+import com.vt.valuetogether.global.validator.CardValidator;
 import com.vt.valuetogether.infra.s3.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -38,5 +41,34 @@ public class CardServiceImpl implements CardService {
 
     private Double getMaxSequence(Long categoryId) {
         return cardRepository.getMaxSequence(categoryId) + NEXT_SEQUENCE;
+    }
+
+    @Override
+    public CardUpdateRes updateCard(CardUpdateReq cardUpdateReq, MultipartFile multipartFile) {
+        Card prevCard = cardRepository.findByCardId(cardUpdateReq.getCardId());
+        CardValidator.validate(prevCard);
+        deleteFile(prevCard.getFileUrl());
+        String fileUrl = null;
+        if (multipartFile != null && !multipartFile.isEmpty()) {
+            fileUrl = s3Util.uploadFile(multipartFile, CARD);
+        }
+        cardRepository.save(
+                Card.builder()
+                        .cardId(prevCard.getCardId())
+                        .name(cardUpdateReq.getName())
+                        .description(cardUpdateReq.getDescription())
+                        .fileUrl(fileUrl)
+                        .sequence(prevCard.getSequence())
+                        .deadline(cardUpdateReq.getDeadline())
+                        .categoryId(prevCard.getCategoryId())
+                        .build());
+        return new CardUpdateRes();
+    }
+
+    private void deleteFile(String fileUrl) {
+        if (fileUrl == null || fileUrl.isBlank()) {
+            return;
+        }
+        s3Util.deleteFile(fileUrl, CARD);
     }
 }
