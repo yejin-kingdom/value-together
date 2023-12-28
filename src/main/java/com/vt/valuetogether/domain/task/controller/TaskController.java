b1a8117f57eb34072ee64b35e5ceccc07771d93e
emost22 <emoney96@naver.com>
2023-12-28 13:03:45 +0900

    [#46] Feat: add update task api

--- src/main/java/com/vt/valuetogether/domain/task/controller/TaskController.java
+++ src/main/java/com/vt/valuetogether/domain/task/controller/TaskController.java
@@ -1,10 +1,13 @@
 package com.vt.valuetogether.domain.task.controller;
 
 import com.vt.valuetogether.domain.task.dto.request.TaskSaveReq;
+import com.vt.valuetogether.domain.task.dto.request.TaskUpdateReq;
 import com.vt.valuetogether.domain.task.dto.response.TaskSaveRes;
+import com.vt.valuetogether.domain.task.dto.response.TaskUpdateRes;
 import com.vt.valuetogether.domain.task.service.TaskService;
 import com.vt.valuetogether.global.response.RestResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,10 @@ public class TaskController {
     @PostMapping
     public RestResponse<TaskSaveRes> saveTask(@RequestBody TaskSaveReq taskSaveReq) {
         return RestResponse.success(taskService.saveTask(taskSaveReq));
+    }
+
+    @PatchMapping
+    public RestResponse<TaskUpdateRes> updateTask(@RequestBody TaskUpdateReq taskUpdateReq) {
+        return RestResponse.success(taskService.updateTask(taskUpdateReq));
     }
 }
--- src/main/java/com/vt/valuetogether/global/validator/TaskValidator.java
+++ src/main/java/com/vt/valuetogether/global/validator/TaskValidator.java
@@ -1,8 +1,11 @@
 package com.vt.valuetogether.global.validator;
 
+import static com.vt.valuetogether.global.meta.ResultCode.NOT_FOUND_TASK;
 import static com.vt.valuetogether.global.meta.ResultCode.NULL_CONTENT;
 
 import com.vt.valuetogether.domain.task.dto.request.TaskSaveReq;
+import com.vt.valuetogether.domain.task.dto.request.TaskUpdateReq;
+import com.vt.valuetogether.domain.task.entity.Task;
 import com.vt.valuetogether.global.exception.GlobalException;
 
 public class TaskValidator {
@@ -10,6 +13,22 @@ public class TaskValidator {
         if (isNullContent(taskSaveReq.getContent())) {
             throw new GlobalException(NULL_CONTENT);
         }
+    }
+
+    public static void validate(TaskUpdateReq taskUpdateReq) {
+        if (isNullContent(taskUpdateReq.getContent())) {
+            throw new GlobalException(NULL_CONTENT);
+        }
+    }
+
+    public static void validate(Task task) {
+        if (isNullTask(task)) {
+            throw new GlobalException(NOT_FOUND_TASK);
+        }
+    }
+
+    private static boolean isNullTask(Task task) {
+        return task == null;
     }
 
     private static boolean isNullContent(String content) {
