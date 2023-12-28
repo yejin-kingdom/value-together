3ce38559d3976bb61edf773e017232a483256932
emost22 <emoney96@naver.com>
2023-12-28 12:33:38 +0900

    [#45] Test: add save task api

---
+++ src/test/java/com/vt/valuetogether/domain/task/controller/TaskControllerTest.java
@@ -0,0 +1,41 @@
+package com.vt.valuetogether.domain.task.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.vt.valuetogether.domain.BaseMvcTest;
+import com.vt.valuetogether.domain.task.dto.request.TaskSaveReq;
+import com.vt.valuetogether.domain.task.dto.response.TaskSaveRes;
+import com.vt.valuetogether.domain.task.service.TaskService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+@WebMvcTest(controllers = {TaskController.class})
+class TaskControllerTest extends BaseMvcTest {
+    @MockBean private TaskService taskService;
+
+    @Test
+    @DisplayName("task 저장 테스트")
+    void task_저장() throws Exception {
+        Long checklistId = 1L;
+        String content = "content";
+        Long taskId = 1L;
+        TaskSaveReq taskSaveReq =
+                TaskSaveReq.builder().checklistId(checklistId).content(content).build();
+        TaskSaveRes taskSaveRes = TaskSaveRes.builder().taskId(taskId).build();
+        when(taskService.saveTask(any())).thenReturn(taskSaveRes);
+        this.mockMvc
+                .perform(
+                        post("/api/v1/tasks")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(taskSaveReq)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
