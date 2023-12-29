fe8fc94f2c052c3d70facd3041ce8c031a3e57d2
emost22 <emoney96@naver.com>
2023-12-30 04:57:21 +0900

    [#95] Test: add save category repo

---
+++ src/test/java/com/vt/valuetogether/domain/category/repository/CategoryRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.vt.valuetogether.domain.category.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.vt.valuetogether.domain.category.entity.Category;
+import com.vt.valuetogether.domain.team.repository.TeamRepository;
+import com.vt.valuetogether.test.CategoryTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CategoryRepositoryTest implements CategoryTest {
+    @Autowired private CategoryRepository categoryRepository;
+    @Autowired private TeamRepository teamRepository;
+
+    @Test
+    @DisplayName("category 저장 테스트")
+    void category_저장() {
+        // given
+        teamRepository.save(TEST_TEAM);
+
+        // when
+        Category category = categoryRepository.save(TEST_CATEGORY);
+
+        // then
+        assertThat(category.getName()).isEqualTo(TEST_CATEGORY_NAME);
+        assertThat(category.getSequence()).isEqualTo(TEST_CATEGORY_SEQUENCE);
+        assertThat(category.getIsDeleted()).isEqualTo(TEST_CATEGORY_IS_DELETED);
+    }
+}
