7f898b5138c9798919072966a907dce162131421
emost22 <emoney96@naver.com>
2023-12-29 11:07:01 +0900

    [#53] Feat: add get card by id api

---
+++ src/main/java/com/vt/valuetogether/domain/card/dto/response/CardGetRes.java
@@ -0,0 +1,36 @@
+package com.vt.valuetogether.domain.card.dto.response;
+
+import com.vt.valuetogether.domain.checklist.dto.response.ChecklistGetRes;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CardGetRes {
+    private Long cardId;
+    private String name;
+    private String description;
+    private String fileUrl;
+    private String deadline;
+    private List<ChecklistGetRes> checklists;
+    // TODO ADD comments, workers
+
+    @Builder
+    private CardGetRes(
+            Long cardId,
+            String name,
+            String description,
+            String fileUrl,
+            String deadline,
+            List<ChecklistGetRes> checklists) {
+        this.cardId = cardId;
+        this.name = name;
+        this.description = description;
+        this.fileUrl = fileUrl;
+        this.deadline = deadline;
+        this.checklists = checklists;
+    }
+}
--- src/main/java/com/vt/valuetogether/domain/card/service/CardService.java
+++ src/main/java/com/vt/valuetogether/domain/card/service/CardService.java
@@ -4,6 +4,7 @@ import com.vt.valuetogether.domain.card.dto.request.CardDeleteReq;
 import com.vt.valuetogether.domain.card.dto.request.CardSaveReq;
 import com.vt.valuetogether.domain.card.dto.request.CardUpdateReq;
 import com.vt.valuetogether.domain.card.dto.response.CardDeleteRes;
+import com.vt.valuetogether.domain.card.dto.response.CardGetRes;
 import com.vt.valuetogether.domain.card.dto.response.CardSaveRes;
 import com.vt.valuetogether.domain.card.dto.response.CardUpdateRes;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,4 +15,6 @@ public interface CardService {
     CardUpdateRes updateCard(CardUpdateReq cardUpdateReq, MultipartFile multipartFile);
 
     CardDeleteRes deleteCard(CardDeleteReq cardDeleteReq);
+
+    CardGetRes getCard(Long cardId);
 }
