9f3914234e7f7721032ad39f1b9dbdbd4b392a9a
barami62 <wodbs7758@naver.com>
2023-12-31 00:08:31 +0900

    [#104] Feat: 토큰에서 username 가져오는 메서드 추가

--- src/main/java/com/vt/valuetogether/global/jwt/JwtUtil.java
+++ src/main/java/com/vt/valuetogether/global/jwt/JwtUtil.java
@@ -110,4 +110,8 @@ public class JwtUtil {
     public Claims getUserInfoFromToken(String token) {
         return jwtParser.parseClaimsJws(token).getBody();
     }
+
+    public String getUsernameFromToken(String token) {
+        return getUserInfoFromToken(token).getSubject();
+    }
 }
