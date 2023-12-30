d167fe99a4048f0919cb8aa27629ddddfbaf7f26
barami62 <wodbs7758@naver.com>
2023-12-30 16:22:01 +0900

    [#77] Fix: 변경된 DTO로 컨트롤러 적용

--- src/main/java/com/vt/valuetogether/domain/worker/controller/WorkerController.java
+++ src/main/java/com/vt/valuetogether/domain/worker/controller/WorkerController.java
@@ -6,7 +6,9 @@ import com.vt.valuetogether.domain.worker.dto.response.WorkerAddRes;
 import com.vt.valuetogether.domain.worker.dto.response.WorkerDeleteRes;
 import com.vt.valuetogether.domain.worker.service.WorkerService;
 import com.vt.valuetogether.global.response.RestResponse;
+import com.vt.valuetogether.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,13 +22,15 @@ public class WorkerController {
 
     private final WorkerService workerService;
 
-    @PostMapping("")
+    @PostMapping
     public RestResponse<WorkerAddRes> addWorker(@RequestBody WorkerAddReq req) {
         return RestResponse.success(workerService.addWorker(req));
     }
 
-    @DeleteMapping("")
-    public RestResponse<WorkerDeleteRes> deleteWorker(@RequestBody WorkerDeleteReq req) {
+    @DeleteMapping
+    public RestResponse<WorkerDeleteRes> deleteWorker(
+            @RequestBody WorkerDeleteReq req, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        req.setUsername(userDetails.getUser().getUsername());
         return RestResponse.success(workerService.deleteWorker(req));
     }
 }
