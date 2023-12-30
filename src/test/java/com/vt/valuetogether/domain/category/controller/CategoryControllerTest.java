613c10ec4a8ea54912c44dfb7ab0114c4e841d2f
emost22 <emoney96@naver.com>
2023-12-31 03:07:49 +0900

    [#106] Test: add get categories api

--- src/test/java/com/vt/valuetogether/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/com/vt/valuetogether/domain/category/controller/CategoryControllerTest.java
@@ -1,19 +1,27 @@
 package com.vt.valuetogether.domain.category.controller;
 
+import static java.lang.Boolean.FALSE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.vt.valuetogether.domain.BaseMvcTest;
+import com.vt.valuetogether.domain.card.dto.response.CardGetRes;
 import com.vt.valuetogether.domain.category.dto.request.CategoryChangeSequenceReq;
 import com.vt.valuetogether.domain.category.dto.request.CategorySaveReq;
 import com.vt.valuetogether.domain.category.dto.response.CategoryChangeSequenceRes;
+import com.vt.valuetogether.domain.category.dto.response.CategoryGetRes;
+import com.vt.valuetogether.domain.category.dto.response.CategoryGetResList;
 import com.vt.valuetogether.domain.category.dto.response.CategorySaveRes;
 import com.vt.valuetogether.domain.category.service.CategoryService;
+import com.vt.valuetogether.domain.checklist.dto.response.ChecklistGetRes;
+import com.vt.valuetogether.domain.task.dto.response.TaskGetRes;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -62,6 +70,61 @@ class CategoryControllerTest extends BaseMvcTest {
                                 .contentType(APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(categoryChangeSequenceReq))
                                 .principal(this.mockPrincipal))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("teamId로 category 전체 조회 테스트")
+    void teamId_category_전체_조회() throws Exception {
+        Long teamId = 1L;
+        Long taskId = 1L;
+        String content = "taskContent";
+        Boolean isCompleted = FALSE;
+        TaskGetRes taskGetRes =
+                TaskGetRes.builder().taskId(taskId).content(content).isCompleted(isCompleted).build();
+        Long checklistId = 1L;
+        String title = "checklistTitle";
+        ChecklistGetRes checklistGetRes =
+                ChecklistGetRes.builder()
+                        .checklistId(checklistId)
+                        .title(title)
+                        .tasks(List.of(taskGetRes))
+                        .build();
+        Long cardId = 1L;
+        String cardName = "cardName";
+        String description = "description";
+        String fileUrl = "fileUrl";
+        Double cardSequence = 1.0;
+        String deadline = "2023.12.31 23:59:59";
+        CardGetRes cardGetRes =
+                CardGetRes.builder()
+                        .cardId(cardId)
+                        .name(cardName)
+                        .description(description)
+                        .fileUrl(fileUrl)
+                        .sequence(cardSequence)
+                        .deadline(deadline)
+                        .checklists(List.of(checklistGetRes))
+                        .build();
+        Long categoryId = 1L;
+        String categoryName = "categoryName";
+        Double categorySequence = 1.0;
+        Boolean isDeleted = FALSE;
+        CategoryGetRes categoryGetRes =
+                CategoryGetRes.builder()
+                        .categoryId(categoryId)
+                        .name(categoryName)
+                        .sequence(categorySequence)
+                        .isDeleted(isDeleted)
+                        .cards(List.of(cardGetRes))
+                        .build();
+        int total = 1;
+        CategoryGetResList categoryGetResList =
+                CategoryGetResList.builder().categories(List.of(categoryGetRes)).total(total).build();
+        when(categoryService.getAllCategories(any(), any())).thenReturn(categoryGetResList);
+        this.mockMvc
+                .perform(get("/api/v1/teams/" + teamId + "/categories").principal(this.mockPrincipal))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
