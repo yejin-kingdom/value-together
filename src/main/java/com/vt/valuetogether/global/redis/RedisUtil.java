df03a30fd576f9d20abd39c3da073e3f8162b01c
barami62 <wodbs7758@naver.com>
2023-12-31 00:13:13 +0900

    [#104] Feat: RedisUtil 로깅 추가

--- src/main/java/com/vt/valuetogether/global/redis/RedisUtil.java
+++ src/main/java/com/vt/valuetogether/global/redis/RedisUtil.java
@@ -2,10 +2,12 @@ package com.vt.valuetogether.global.redis;
 
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Component;
 
+@Slf4j(topic = "Redis Util")
 @Component
 @RequiredArgsConstructor
 public class RedisUtil {
@@ -14,12 +16,16 @@ public class RedisUtil {
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void set(String key, Object o, int minutes) {
+        log.info("[set] key : {}, value : {}, time : {} minutes", key, o, minutes);
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(o.getClass()));
         redisTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
     }
 
     public Object get(String key) {
-        return redisTemplate.opsForValue().get(key);
+        log.info("[get] key : {}", key);
+        Object value = redisTemplate.opsForValue().get(key);
+        log.info("[get] value : {}", value);
+        return value;
     }
 
     public boolean delete(String key) {
@@ -30,6 +36,9 @@ public class RedisUtil {
     }
 
     public boolean hasKey(String key) {
-        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+        log.info("[hasKey] key : {}", key);
+        boolean hasKey = Boolean.TRUE.equals(redisTemplate.hasKey(key));
+        log.info("[hasKey] hasKey : {}", hasKey);
+        return hasKey;
     }
 }
