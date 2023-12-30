e766f282dfd397dcec31c1802fb0e22bf5d93ca9
barami62 <wodbs7758@naver.com>
2023-12-30 16:19:32 +0900

    [#77] Fix: 복합키 적용

--- src/main/java/com/vt/valuetogether/domain/worker/entity/Worker.java
+++ src/main/java/com/vt/valuetogether/domain/worker/entity/Worker.java
@@ -2,11 +2,12 @@ package com.vt.valuetogether.domain.worker.entity;
 
 import com.vt.valuetogether.domain.card.entity.Card;
 import com.vt.valuetogether.domain.user.entity.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -16,23 +17,23 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@IdClass(WorkerPK.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tb_worker")
 public class Worker {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long workerId;
-
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "cardId")
     private Card card;
 
     @Builder
-    private Worker(Long workerId, User user, Card card) {
-        this.workerId = workerId;
+    private Worker(User user, Card card) {
         this.user = user;
         this.card = card;
     }
