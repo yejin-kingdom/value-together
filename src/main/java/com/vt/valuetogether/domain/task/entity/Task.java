e199262ba95a807ea76abbba61f674d587f6cabd
junxtar <aa1870088@naver.com>
2023-12-31 16:41:20 +0900

    [#121] Feat: task -> checklist 연관관계 삭제 추가

--- src/main/java/com/vt/valuetogether/domain/task/entity/Task.java
+++ src/main/java/com/vt/valuetogether/domain/task/entity/Task.java
@@ -14,6 +14,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -32,6 +34,7 @@ public class Task extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "checklistId")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Checklist checklist;
 
     @Builder
