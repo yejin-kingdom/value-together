bbd564d4dd5cd57154fc9b94fe7e0bf886d02e29
leeyejin <leeyejin113@gmail.com>
2023-12-31 19:05:26 +0900

    [#118] Test: add MailUtilTest

--- src/test/java/com/vt/valuetogether/test/EmailAuthTest.java
+++ src/test/java/com/vt/valuetogether/test/EmailAuthTest.java
@@ -2,10 +2,12 @@ package com.vt.valuetogether.test;
 
 import com.vt.valuetogether.domain.user.entity.EmailAuth;
 
-public interface EmailAuthTest {
+public interface EmailAuthTest extends TeamTest, UserTest {
     String TEST_EMAIL = "test@gmail.com";
 
     String TEST_CODE = "ABC123";
+
+    String TEST_ANOTHER_CODE = "EFG456";
 
     EmailAuth TEST_EMAIL_AUTH =
             EmailAuth.builder().email(TEST_EMAIL).code(TEST_CODE).isChecked(false).build();
