c0e7b675b9e1e6380ef286ebcc39119689f4b71e
emost22 <emoney96@naver.com>
2024-01-01 20:52:59 +0900

    [#153] Fix: Worker, TemRole entity

--- src/main/java/com/vt/valuetogether/domain/worker/service/impl/WorkerServiceImpl.java
+++ src/main/java/com/vt/valuetogether/domain/worker/service/impl/WorkerServiceImpl.java
@@ -2,6 +2,8 @@ package com.vt.valuetogether.domain.worker.service.impl;
 
 import com.vt.valuetogether.domain.card.entity.Card;
 import com.vt.valuetogether.domain.card.repository.CardRepository;
+import com.vt.valuetogether.domain.team.entity.TeamRole;
+import com.vt.valuetogether.domain.team.repository.TeamRoleRepository;
 import com.vt.valuetogether.domain.user.entity.User;
 import com.vt.valuetogether.domain.user.repository.UserRepository;
 import com.vt.valuetogether.domain.worker.dto.request.WorkerAddReq;
@@ -13,6 +15,7 @@ import com.vt.valuetogether.domain.worker.repository.WorkerRepository;
 import com.vt.valuetogether.domain.worker.service.WorkerService;
 import com.vt.valuetogether.domain.worker.service.WorkerServiceMapper;
 import com.vt.valuetogether.global.validator.CardValidator;
+import com.vt.valuetogether.global.validator.TeamRoleValidator;
 import com.vt.valuetogether.global.validator.UserValidator;
 import com.vt.valuetogether.global.validator.WorkerValidator;
 import lombok.RequiredArgsConstructor;
@@ -26,20 +29,24 @@ public class WorkerServiceImpl implements WorkerService {
     private final UserRepository userRepository;
     private final CardRepository cardRepository;
     private final WorkerRepository workerRepository;
+    private final TeamRoleRepository teamRoleRepository;
 
     @Transactional
     @Override
     public WorkerAddRes addWorker(WorkerAddReq req) {
-        User user = findUser(req.getUsername());
-        Card card = findCard(req.getCardId());
+        Card card = findCard(req.getCardId(), req.getUsername());
+        TeamRole teamRole =
+                findTeamRole(req.getAddUsername(), card.getCategory().getTeam().getTeamId());
 
         return WorkerServiceMapper.INSTANCE.toWorkerAddRes(
-                workerRepository.save(Worker.builder().user(user).card(card).build()));
+                workerRepository.save(Worker.builder().teamRole(teamRole).card(card).build()));
     }
 
-    private Card findCard(Long cardId) {
+    private Card findCard(Long cardId, String username) {
+        User user = findUser(username);
         Card card = cardRepository.findByCardId(cardId);
         CardValidator.validate(card);
+        TeamRoleValidator.checkIsTeamMember(card.getCategory().getTeam().getTeamRoleList(), user);
         return card;
     }
 
@@ -47,6 +54,12 @@ public class WorkerServiceImpl implements WorkerService {
         User user = userRepository.findByUsername(username);
         UserValidator.validate(user);
         return user;
+    }
+
+    private TeamRole findTeamRole(String username, Long teamId) {
+        TeamRole teamRole = teamRoleRepository.findByUserUsernameAndTeamTeamId(username, teamId);
+        TeamRoleValidator.validate(teamRole);
+        return teamRole;
     }
 
     @Transactional
@@ -57,10 +70,11 @@ public class WorkerServiceImpl implements WorkerService {
     }
 
     private Worker findWorker(WorkerDeleteReq req) {
-        User user = findUser(req.getUsername());
-        Card card = findCard(req.getCardId());
+        Card card = findCard(req.getCardId(), req.getUsername());
+        TeamRole teamRole =
+                findTeamRole(req.getDeleteUsername(), card.getCategory().getTeam().getTeamId());
 
-        Worker worker = workerRepository.findByUserAndCard(user, card);
+        Worker worker = workerRepository.findByTeamRoleAndCard(teamRole, card);
         WorkerValidator.validator(worker);
         return worker;
     }
--- src/test/java/com/vt/valuetogether/test/TeamRoleTest.java
+++ src/test/java/com/vt/valuetogether/test/TeamRoleTest.java
@@ -6,7 +6,6 @@ import com.vt.valuetogether.domain.team.entity.TeamRole;
 public interface TeamRoleTest extends TeamTest, UserTest {
     Long TEST_TEAM_ROLE_ID = 1L;
     Role TEST_TEAM_ROLE_ROLE = Role.LEADER;
-    boolean TEST_TEAM_ROLE_IS_DELETED = false;
 
     TeamRole TEST_TEAM_ROLE =
             TeamRole.builder()
@@ -14,6 +13,5 @@ public interface TeamRoleTest extends TeamTest, UserTest {
                     .user(TEST_USER)
                     .team(TEST_TEAM)
                     .role(TEST_TEAM_ROLE_ROLE)
-                    .isDeleted(TEST_TEAM_ROLE_IS_DELETED)
                     .build();
 }
