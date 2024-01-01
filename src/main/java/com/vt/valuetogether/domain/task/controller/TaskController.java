d043c2ece2d63bd1335150804f717527533f89dc
emost22 <emoney96@naver.com>
2024-01-01 23:10:20 +0900

    [#169] Fix: add teamrole validator in save task api

--- src/main/java/com/vt/valuetogether/domain/task/controller/TaskController.java
+++ src/main/java/com/vt/valuetogether/domain/task/controller/TaskController.java
@@ -9,6 +9,8 @@ import com.vt.valuetogether.domain.task.dto.response.TaskUpdateRes;
 import com.vt.valuetogether.domain.task.service.TaskService;
 import com.vt.valuetogether.global.response.RestResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +25,9 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping
-    public RestResponse<TaskSaveRes> saveTask(@RequestBody TaskSaveReq taskSaveReq) {
+    public RestResponse<TaskSaveRes> saveTask(
+            @RequestBody TaskSaveReq taskSaveReq, @AuthenticationPrincipal UserDetails userDetails) {
+        taskSaveReq.setUsername(userDetails.getUsername());
         return RestResponse.success(taskService.saveTask(taskSaveReq));
     }
 
