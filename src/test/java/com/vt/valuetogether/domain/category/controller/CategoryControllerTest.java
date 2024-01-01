f671a4ae05d25775a8a5d07ba49eaf7b5efe0177
barami62 <wodbs7758@naver.com>
2024-01-02 00:38:36 +0900

    [#151] Test: 카테고리 이름 수정 테스트 추가

--- src/test/java/com/vt/valuetogether/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/com/vt/valuetogether/domain/category/controller/CategoryControllerTest.java
@@ -1,6 +1,7 @@
 package com.vt.valuetogether.domain.category.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,13 +13,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.vt.valuetogether.domain.BaseMvcTest;
 import com.vt.valuetogether.domain.card.dto.response.CardInnerCategoryRes;
 import com.vt.valuetogether.domain.category.dto.request.CategoryChangeSequenceReq;
+import com.vt.valuetogether.domain.category.dto.request.CategoryEditReq;
 import com.vt.valuetogether.domain.category.dto.request.CategorySaveReq;
 import com.vt.valuetogether.domain.category.dto.response.CategoryChangeSequenceRes;
+import com.vt.valuetogether.domain.category.dto.response.CategoryEditRes;
 import com.vt.valuetogether.domain.category.dto.response.CategoryGetRes;
 import com.vt.valuetogether.domain.category.dto.response.CategoryGetResList;
 import com.vt.valuetogether.domain.category.dto.response.CategorySaveRes;
 import com.vt.valuetogether.domain.category.service.CategoryService;
 import com.vt.valuetogether.domain.worker.dto.response.WorkerGetRes;
+import com.vt.valuetogether.test.CategoryTest;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,18 +30,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @WebMvcTest(controllers = {CategoryController.class})
-class CategoryControllerTest extends BaseMvcTest {
+class CategoryControllerTest extends BaseMvcTest implements CategoryTest {
+
     @MockBean private CategoryService categoryService;
 
     @Test
     @DisplayName("category 저장 테스트")
     void category_저장() throws Exception {
-        Long teamId = 1L;
-        String name = "category";
-        CategorySaveReq categorySaveReq = CategorySaveReq.builder().teamId(teamId).name(name).build();
-        Long categoryId = 1L;
-        CategorySaveRes categorySaveRes = CategorySaveRes.builder().categoryId(categoryId).build();
+        CategorySaveReq categorySaveReq =
+                CategorySaveReq.builder().teamId(TEST_TEAM_ID).name(TEST_CATEGORY_NAME).build();
+        CategorySaveRes categorySaveRes =
+                CategorySaveRes.builder().categoryId(TEST_CATEGORY_ID).build();
+
         when(categoryService.saveCategory(any())).thenReturn(categorySaveRes);
+
         this.mockMvc
                 .perform(
                         post("/api/v1/categories")
@@ -51,17 +57,19 @@ class CategoryControllerTest extends BaseMvcTest {
     @Test
     @DisplayName("category 순서 변경 테스트")
     void category_순서_변경() throws Exception {
-        Long categoryId = 1L;
         Double preSequence = 2.0;
         Double postSequence = 3.0;
         CategoryChangeSequenceReq categoryChangeSequenceReq =
                 CategoryChangeSequenceReq.builder()
-                        .categoryId(categoryId)
+                        .categoryId(TEST_CATEGORY_ID)
                         .preSequence(preSequence)
                         .postSequence(postSequence)
                         .build();
+
         CategoryChangeSequenceRes categoryChangeSequenceRes = new CategoryChangeSequenceRes();
+
         when(categoryService.changeCategorySequence(any())).thenReturn(categoryChangeSequenceRes);
+
         this.mockMvc
                 .perform(
                         patch("/api/v1/categories/order")
@@ -111,7 +119,31 @@ class CategoryControllerTest extends BaseMvcTest {
                 CategoryGetResList.builder().categories(List.of(categoryGetRes)).total(total).build();
         when(categoryService.getAllCategories(any(), any())).thenReturn(categoryGetResList);
         this.mockMvc
-                .perform(get("/api/v1/teams/" + teamId + "/categories").principal(this.mockPrincipal))
+                .perform(get("/api/v1/teams/{teamId}/categories", teamId).principal(this.mockPrincipal))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("카테고리 이름 수정")
+    void category_이름_수정() throws Exception {
+        CategoryEditReq req =
+                CategoryEditReq.builder()
+                        .categoryId(TEST_CATEGORY_ID)
+                        .name(TEST_ANOTHER_CATEGORY_NAME)
+                        .build();
+        req.setName(this.mockPrincipal.getName());
+
+        CategoryEditRes res = new CategoryEditRes();
+
+        given(categoryService.editCategory(any(CategoryEditReq.class))).willReturn(res);
+
+        this.mockMvc
+                .perform(
+                        patch("/api/v1/categories")
+                                .principal(this.mockPrincipal)
+                                .contentType(APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
