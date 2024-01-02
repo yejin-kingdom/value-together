248a84ef4d973f5a247ae4206b66fa04c2fc4e8f
yongcowhee <yongcowhee@gmail.com>
2024-01-02 15:05:40 +0900

    [#188] Feat: TeamRestoreReq 필드 및 builder 추가

--- src/main/java/com/vt/valuetogether/domain/team/dto/request/TeamRestoreReq.java
+++ src/main/java/com/vt/valuetogether/domain/team/dto/request/TeamRestoreReq.java
@@ -1,5 +1,19 @@
 package com.vt.valuetogether.domain.team.dto.request;
 
-public class TeamRestoreReq {
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamRestoreReq {
+    private Long teamId;
+    private String username;
+
+    @Builder
+    private TeamRestoreReq(Long teamId, String username) {
+        this.teamId = teamId;
+        this.username = username;
+    }
 }
